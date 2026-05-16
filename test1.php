9d7c144af8487fe8c325c20693d4720862f91215
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-17 00:20:32 +0100

    Pour chaque utilisateur, affiche son user_id et le nombre total de commandes qu’il a passées. Dans une page php .

--- test1.php
+++ test1.php
@@ -3,7 +3,32 @@
 $pdo= new PDO("mysql:host=localhost;dbname=client_fidèl","root","" );
 
 
+$sql=" 
+Select user_id, COUNT(*) as Total_commandes
+From orders
+GROUP BY user_id
+";
+
+$stmt= $pdo->query($sql);
+$clients=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 
 
 ?>
+
+<h2>Clients fidèles</h2>
+
+<table border="1">
+<tr>
+    <th>User ID</th>
+    <th>Nombre de commandes</th>
+</tr>
+
+<?php foreach ($clients as $client): ?>
+<tr>
+    <td><?= $client['user_id'] ?></td>
+    <td><?= $client['Total_commandes'] ?></td>
+</tr>
+<?php endforeach; ?>
+</table>
